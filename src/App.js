f14358677b86e1005acb00f0fdd95a49081128e3
tanujn45 <tanujn45@gmail.com>
2021-01-29 18:15:22 +0530

    hero page complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Header from './components/Header';
 import Button from './components/Button'
+import cycle from './assets/img/cycle.png'
 
 const App = () => {
   return <>
     <Header />
-    <div id="hero">
+    <section id="hero">
       <div className="container">
-        <h3>Ride the</h3>
-        <h1>Electric<br></br>Revolution</h1>
-        <Button text="Explore models" />
+        <div className="hero-container">
+          <div className="hero-text">
+            <h3>Ride the</h3>
+            <h1>Electric<br></br>Revolution</h1>
+            <Button text="Explore models" />
+          </div>
+          <div className="hero-model">
+            <img className="img-fluid" src={cycle} alt="" />
+            <p>Drag.zoom.move</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
 
     <div id="about"></div>
   </>
