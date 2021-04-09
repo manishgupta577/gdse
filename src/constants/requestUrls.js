a17b53df0acce8d7077260326ff554518f92eb66
tanujn45 <tanujn45@gmail.com>
2021-04-10 03:41:10 +0530

    changes

--- src/constants/requestUrls.js
+++ src/constants/requestUrls.js
@@ -1,6 +1,6 @@
 const requestUrls = {
-  base_url: "http://ec2-13-127-223-82.ap-south-1.compute.amazonaws.com/v1/", //prod
-  // base_url: "http://ec2-13-233-252-28.ap-south-1.compute.amazonaws.com/v1/", //dev
+  // base_url: "http://ec2-13-127-223-82.ap-south-1.compute.amazonaws.com/v1/", //prod
+  base_url: "http://ec2-13-233-252-28.ap-south-1.compute.amazonaws.com/v1/", //dev
   // base_url: "http://localhost:8000/v1/", // localhost
   product_list: "product/list",
   category_list: "category/list",
